461005191f6455ca629e5cc7be1152bc4606c45f
Rahul Prajapati <rohngonnarock@gmail.com>
2015-07-31 01:44:40 +0530

    first commit

---
+++ app/src/main/java/com/godnsons/com/godnsons/Main.java
@@ -0,0 +1,120 @@
+package com.godnsons.com.godnsons;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.AdView;
+
+
+public class Main extends ActionBarActivity {
+
+    private InterstitialAd mInterstitialAd;
+    private CountDownTimer mCountDownTimer;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+
+        // Create the InterstitialAd and set the adUnitId.
+        mInterstitialAd = new InterstitialAd(this);
+        // Defined in res/values/strings.xml
+        mInterstitialAd.setAdUnitId(getString(R.string.Interstital_ad_unit_id));
+
+        String url = "http://godnsons.com/";
+        WebView view=(WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+        mCountDownTimer = new CountDownTimer(100000, 50) {
+            @Override
+            public void onTick(long millisUnitFinished) {
+                //textView.setText("seconds remaining: " + ((millisUnitFinished / 1000) + 1));
+            }
+
+            @Override
+            public void onFinish() {
+                //textView.setText("done!");
+                //mRetryButton.setVisibility(View.VISIBLE);
+                showInterstitial();
+            }
+        };
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        // Initialize the timer if it hasn't been initialized yet.
+        // Start the game.
+        super.onResume();
+        startGame();
+    }
+
+    @Override
+    public void onPause() {
+        // Cancel the timer if the game is paused.
+        mCountDownTimer.cancel();
+        super.onPause();
+    }
+
+    private void showInterstitial() {
+        // Show the ad if it's ready. Otherwise toast and restart the game.
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            //Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+            startGame();
+        }
+    }
+
+    private void startGame() {
+        // Hide the retry button, load the ad, and start the timer.
+        //mRetryButton.setVisibility(View.INVISIBLE);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mInterstitialAd.loadAd(adRequest);
+        mCountDownTimer.start();
+    }
+}
